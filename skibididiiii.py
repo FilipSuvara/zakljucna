c913efa71e3dd364493e4ad6e51d3b21c45e9f94
FilipSuvara <filip.suvara25112012>
2024-05-28 16:34:23 +0200

    skibidi

---
+++ skibididiiii.py
@@ -0,0 +1,10 @@
+ocena1=int(input("reci pet ocena"))
+ocena2=int(input("reci pet ocena"))
+ocena3=int(input("reci pet ocena"))
+ocena4=int(input("reci pet ocena"))
+ocena5=int(input("reci pet ocena"))
+
+ocenas=ocena1+ocena2+ocena3+ocena4+ocena5
+ocenad=ocenas/5
+print("zakljucna je ",round(ocenad , 2))
+
